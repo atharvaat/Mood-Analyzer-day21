f52bcae964b5a39275a829ba55451a71b563fdf8
atharva <atharvatodankar.at@gmail.com>
2022-12-14 22:01:30 +0530

    Mood Analyzer Problem

---
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+public class MoodAnalyzer {
+     //Parameterized Constructor
+     public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    //Handle Customized Exception using try-catch block
+    public String analyseMood() throws MoodAnalysisException {
+        try {
+            if (message.length() == 0)
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY , "Please enter proper message!");
+            if (message.contains("sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch(NullPointerException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL , "Please enter proper message!");
+        }
+    }
+}
+    
